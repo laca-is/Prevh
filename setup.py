36d56c6811620b6fbec93bfee933e6c42dd732bf
Julio César G. Costa <51680369+JCGCosta@users.noreply.github.com>
2021-09-29 14:55:51 -0300

    file update
    file name changed for publication

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Education',
     'Operating System :: Microsoft :: Windows :: Windows 10',
-    'License :: OSI Approved :: CC0-1.0 License',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.8'
 ]
 
@@ -12,11 +12,11 @@ setup(
     name='prevh',
     version='0.0.1',
     description='A data analysis library for data mining.',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='',
     author='Júlio César Guimarães Costa',
     author_email='juliocesargcosta123@gmail.com',
-    license='CC0-1.0 License',
+    license='MIT License',
     classifiers=classifiers,
     keywords='Data Mining',
     packages=find_packages(),
